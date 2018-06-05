6326e630744c9ee365c62fba0fe34766ee644588
Karen Brant <karen.brant3@gmail.com>
2018-06-05 18:36:54 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,169 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+// var axios = require("axios");
+var cheerio = require("cheerio");
+var request = require("request");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// var routes = require("./controllers/articlesController.js");
+// app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/populateMongo");
+
+// Routes
+
+// A GET route for scraping the Richmond Times Dispatch website
+app.get("/scrape", function(req, res, error) {
+  // First, we grab the body of the html with request
+  if (error) {
+    console.log(error);
+  }
+  // axios.get("http://www.richmond.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    // var $ = cheerio.load(response.data);
+    request("http://www.richmond.com", function(error, response, html) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(html);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // Select each element in the HTML body from which you want information.
+  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+  // but be sure to visit the package's npm page to see how it works
+  $("div.card-body").each(function(i, element) {
+
+    var link = $(element).children().attr("href");
+    var title;
+    var summary;
+    if ($(element).find("h3")) {
+      debugger;
+      console.log("element.find is h3");
+      title = $(element).find("h3");
+      summary = $("div.card-lead");
+    } else {
+      title = $(element).find("h4");
+    }
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title: "test",
+      link: link,
+      summary: summary
+    });
+    console.log(results);
+  });
+    // Now, we grab every h2 within an article tag, and do the following:
+    // $("h3.tnt-headline").each(function(i, element) {
+    //   // Save an empty result object
+    //   var result = {};
+    //   console.log("Gets to /scrape function");
+    //   // Add the text and href of every link, and save them as properties of the result object
+    //   result.title = $(this)
+    //     .children("a")
+    //     .text();
+    //   result.link = $(this)
+    //     .children("a")
+    //     .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(results)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          // console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
+  });
+
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
