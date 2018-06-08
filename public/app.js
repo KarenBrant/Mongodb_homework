40b2d2192b013817ee01cb3322b8a3521c794b12
Karen Brant <karen.brant3@gmail.com>
2018-06-08 12:56:07 -0400

    Changed news station and added modal for notes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Grab the articles as a json
-
+var limit = 50;
 $(".scrape-new").on("click", function() {
     
   $.ajax({
@@ -10,20 +10,38 @@ $(".scrape-new").on("click", function() {
   .then(function(data) {
                 $.getJSON("/articles", function(data) {
     // For each one
-            for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-                console.log("ID: " + data[i]._id);
-                $("#articles").append("<h3 data-id='" + data[i]._id + "'>" + data[i].title + "<a class='article-link' target='blank'>" + "href=" + data[i].link + "</a>" + "</h3>");
+            for (var i = 0; i < limit; i++) {
+      // Display the apropos information on the page 
+                console.log("id: " + data[i]._id);
+                $("#articles").append("<h3 <a data-id='" + data[i]._id + "'>" + "Title:  " + data[i].title + "<br>" + "Link:  " + data[i].link + "</a>" + "</h3>");
                 $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].summary + "</p>");
-            }
+                // $("#articles").append("<button type='button' id='note-article'>Article Notes</button>" + "<br> <br>");
+                $("#articles").append("<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#modalCenter'>Article Notes</button>");
+
+              }
         });
 });
 });
   
-  
+// $(document).on("click", ".btn.save", handleArticleSave);
+
+//   var artSave = $(this).parents(".panel").data();
+//   artSave.saved = true;
+//   $.ajax({
+//     method: "PUT",
+//     url: ,
+//     data: artSave
+//   }).then(function(data){
+//     if (data.ok) {
+//       initPage();
+//     }
+//   });
+
+
   // Whenever someone clicks a save button
-  $(".save").on("click", function() {
+  $("#note-article").on("click", function() {
     // Empty the notes from the note section
+    console.log("got to notes");
     $("#notes").empty();
     // Save the id from the p tag
     var thisId = $(this).attr("data-id");
@@ -37,14 +55,32 @@ $(".scrape-new").on("click", function() {
       .then(function(data) {
         console.log(data);
         // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
+        // $("#notes").append("<div class='modal' tabindex='-1' role='dialog'>");
+        // $("#notes").append("<div class='modal-dialog' role='document'>");
+        // $("#notes").append("<div class='modal-content'>");
+        // $("#notes").append("<div class='modal-header'>");
+        // $("#notes").append("<h5 class='modal-title'>" + data.title + "</h5>");
+        // $("#notes").append("<button type='button' class='close' data-dismiss='modal' aria-label='Close'>");
+        // $("#notes").append("<span aria-hidden='true'>&times;</span></button></div>");
+        // $("#notes").append("<div class='modal-body>");
+        // // An input to enter a new title
+        $('#modalCenter').on('show.bs.modal', function (event) {
+          var button = $(event.relatedTarget) // Button that triggered the modal
+          var recipient = button.data('target') // Extract info from data-* attributes
+          // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+          // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+          var modal = $(this)
+          modal.find('.modal-title').text('New message to ' + recipient)
+          modal.find('.modal-body input').val(recipient)
+        })
+        $(".modal-body").append("<p id='titleinput'>" + data.title + "</p>");
+        // // A textarea to add a new note body
+        $(".modal-body").append("<p id='bodyinput'>" + data.notes + "</p></div>");
+        // // A button to submit a new note, with the id of the article saved to it
+        // $("#notes").append("<div class='modal-footer'>");
+        // $("#notes").append("<button data-id='" + data._id + "' id='savenote' type='button' class='btn btn-primary'>Save Note</button>");
+        // $("#notes").append("<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>");
+        // $("#notes").append("</div></div></div></div>");
         // If there's a note in the article
         if (data.note) {
           // Place the title of the note in the title input
--- server.js
+++ server.js
@@ -28,7 +28,9 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
@@ -41,48 +43,44 @@ mongoose.connect("mongodb://localhost/populateMongo");
 // Routes
 
 // A GET route for scraping the Richmond Times Dispatch website
-app.get("/scrape", function(req, res, error) {
+app.get("/scrape", function (req, res, error) {
   // First, we grab the body of the html with request
   if (error) {
     console.log(error);
   }
   // axios.get("http://www.richmond.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    // var $ = cheerio.load(response.data);
-    request("http://www.richmond.com", function(error, response, html) {
+  // Then, we load that into cheerio and save it to $ for a shorthand selector
+  // var $ = cheerio.load(response.data);
+  // request("http://www.richmond.com", function (error, response, html) {
+  request("http://www.wric.com", function (error, response, html) {
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(html);
+    console.log ("did we get here");
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
+    // An empty array to save the data that we'll scrape
+    var results = [];
 
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("div.card-body").each(function(i, element) {
+    // Select each element in the HTML body from which you want information.
+    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+    // but be sure to visit the package's npm page to see how it works
+    $("div.headline-wrapper").each(function (i, element) {
 
-    var link = $(element).children().attr("href");
-    var title;
-    var summary;
-    if ($(element).find("h3")) {
-      debugger;
-      console.log("element.find is h3");
-      title = $(element).find("h3");
-      summary = $("div.card-lead");
-    } else {
-      title = $(element).find("h4");
-    }
+      var link = $(element).children("a").attr("href");
+      var title = $(element).children("h4.headline").text();
+      var summary = $(element).children("p").text();
+    
 
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: "test",
-      link: link,
-      summary: summary
+      // Save these results in an object that we'll push into the results array we defined earlier
+      results.push({
+        title: title,
+        link: link,
+        summary: summary
+      });
+      console.log("how about here");
+      console.log(results);
     });
-    console.log(results);
-  });
     // Now, we grab every h2 within an article tag, and do the following:
     // $("h3.tnt-headline").each(function(i, element) {
     //   // Save an empty result object
@@ -96,74 +94,84 @@ app.get("/scrape", function(req, res, error) {
     //     .children("a")
     //     .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(results)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          // console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
-    });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
+    // Create a new Article using the `result` object built from scraping
+    db.Article.create(results)
+      .then(function (dbArticle) {
+        // View the added result in the console
+        // console.log(dbArticle);
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        // If an error occurred, send it to the client
+        return res.json(err);
+      });
   });
+
+  // If we were able to successfully scrape and save an Article, send a message to the client
+  // res.send("Scrape Complete");
+  res.end;
+});
 
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
+  db.Article.findOne({
+      _id: req.params.id
+    })
     // ..and populate all of the notes associated with it
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(function (dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate({
+        _id: req.params.id
+      }, {
+        note: dbNote._id
+      }, {
+        new: true
+      });
     })
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
